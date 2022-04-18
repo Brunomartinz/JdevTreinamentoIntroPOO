cd2d4a576ad3a7b3b17c9818bad91cbeeb455fe6
Brunomartinz <brunomartins97@Outlook.com>
2022-04-18 00:30:39 -0300

    Criando atributos de disciplina do aluno3

--- src/cursojava/classes/Aluno.java
+++ src/cursojava/classes/Aluno.java
@@ -12,9 +12,14 @@ public class Aluno {
 	private String cpf;
 
 	private double nota1;
+	private String disciplina1;
 	private double nota2;
+	private String disciplina2;
 	private double nota3;
+	private String disciplina3;
 	private double nota4;
+	private String disciplina4;
+	
 
 	public Aluno() {
 
@@ -98,6 +103,38 @@ public class Aluno {
 		return (nota1 + nota2 + nota3 + nota4) / 4;
 	}
 
+	public String getDisciplina1() {
+		return disciplina1;
+	}
+
+	public void setDisciplina1(String disciplina1) {
+		this.disciplina1 = disciplina1;
+	}
+
+	public String getDisciplina2() {
+		return disciplina2;
+	}
+
+	public void setDisciplina2(String disciplina2) {
+		this.disciplina2 = disciplina2;
+	}
+
+	public String getDisciplina3() {
+		return disciplina3;
+	}
+
+	public void setDisciplina3(String disciplina3) {
+		this.disciplina3 = disciplina3;
+	}
+
+	public String getDisciplina4() {
+		return disciplina4;
+	}
+
+	public void setDisciplina4(String disciplina4) {
+		this.disciplina4 = disciplina4;
+	}
+
 	public boolean getAlunoAprovado() {
 		double media = this.getMediaNotas();
 		if (media >= 70) {
@@ -107,10 +144,13 @@ public class Aluno {
 		}
 	}
 
+
 	@Override
 	public String toString() {
 		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", cpf=" + cpf
-				+ ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + ", nota4=" + nota4 + "]";
+				+ ", nota1=" + nota1 + ", disciplina1=" + disciplina1 + ", nota2=" + nota2 + ", disciplina2="
+				+ disciplina2 + ", nota3=" + nota3 + ", disciplina3=" + disciplina3 + ", nota4=" + nota4
+				+ ", disciplina4=" + disciplina4 + "]";
 	}
 
 	@Override
--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,17 +7,26 @@ import cursojava.classes.Aluno;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-/*
+
 		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		String idade = JOptionPane.showInputDialog("Qual a idade ?");
 		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno ?");
+		String disciplina1 = JOptionPane.showInputDialog("Informe o nome da disciplina 1: ");
 		String nota1 = JOptionPane.showInputDialog("Nota 1: ");
+		String disciplina2 = JOptionPane.showInputDialog("Informe o nome da disciplina 2: ");
 		String nota2 = JOptionPane.showInputDialog("Nota 2: ");
+		String disciplina3 = JOptionPane.showInputDialog("Informe o nome da disciplina 3: ");
 		String nota3 = JOptionPane.showInputDialog("Nota 3: ");
+		String disciplina4 = JOptionPane.showInputDialog("Informe o nome da disciplina : ");
 		String nota4 = JOptionPane.showInputDialog("Nota 4: ");
-
 	
 		Aluno aluno1 = new Aluno();
+		
+		aluno1.setDisciplina1(disciplina1);
+		aluno1.setDisciplina2(disciplina2);
+		aluno1.setDisciplina3(disciplina3);
+		aluno1.setDisciplina4(disciplina4);
+		
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
@@ -26,28 +35,9 @@ public class PrimeiraClasseJava {
 		aluno1.setNota3(Integer.valueOf(nota3));
 		aluno1.setNota4(Integer.valueOf(nota4));
 		
-	
 		System.out.println(aluno1.toString());
 		System.out.println("Média do aluno = "+aluno1.getMediaNotas());
 		System.out.println("Resultado = "+aluno1.getAlunoAprovado());
-		*/
-
-		/*Equal e Hascode ( Diferenciar e comparar Objetos) */
-		
-		Aluno aluno2 = new Aluno();
-		aluno2.setNome("Lola");
-		aluno2.setCpf("44469914851");
-		
-		Aluno aluno3 = new Aluno();
-		aluno3.setNome("Lola");
-		aluno2.setCpf("44469914851");
-		
-		if(aluno2.equals(aluno3)) {
-			System.out.println("alunos são iguais");
-		}else {
-			System.out.println("alunos não são iguais");
-		}
-		
 	
 	}
 
