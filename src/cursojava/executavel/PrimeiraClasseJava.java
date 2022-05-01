6142b42eae071384d590f05bc5a2285d1ff1b57b
Brunomartinz <brunomartins97@Outlook.com>
2022-05-01 19:51:57 -0300

    Removendo disciplinas da lista do aluno

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -10,35 +10,42 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 
 		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
-	    String idade = JOptionPane.showInputDialog("Qual a idade ?");
+		String idade = JOptionPane.showInputDialog("Qual a idade ?");
 		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno ?");
 		String cpf = JOptionPane.showInputDialog("Qual o cpf do aluno(a) ? ");
-		
-	
+
 		Aluno aluno1 = new Aluno();
-		
+
 		aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setCpf(cpf);
-		
-		
-		for(int pos = 1; pos <= 4; pos++) {
-			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina "+pos+": ");
-			String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina "+pos+":");
-			
+
+		for (int pos = 1; pos <= 4; pos++) {
+
+			String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina " + pos + ": ");
+			String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina " + pos + ":");
+
 			Disciplina disciplina = new Disciplina();
 			disciplina.setDisciplina(nomeDisciplina);
 			disciplina.setNota(Double.valueOf(notaDisciplina));
-			
+
 			aluno1.getDisciplinas().add(disciplina);
 		}
 		
+		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ? ");
 		
+		if( escolha == 0 ) {
+			String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina? ");
+			aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - 1);
+			
+		}
+		
+
 		System.out.println(aluno1.toString());
-		System.out.println("Média do aluno = "+aluno1.getMediaNotas());
-		System.out.println("Resultado = "+aluno1.getAlunoAprovado());
-	
+		System.out.println("Média do aluno = " + aluno1.getMediaNotas());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado());
+
 	}
 
 }
